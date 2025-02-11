9cab1b23649ec159a0d7ab3e0bb57167c94fd082
Ayan Hazarika <ayhazarika@gmail.com>
2025-02-10 19:18:54 -0500

    Updated speed control with magnitude and direction functions

--- firmware/RC-Firmware/src/speed_control.h
+++ firmware/RC-Firmware/src/speed_control.h
@@ -8,11 +8,16 @@
 #define RUMBLE_CNTL GPIO_NUM_12 //Arbitrary GPIO 
 #define MODE_CNTL GPIO_NUM_13 //Arbitrary GPIO
 #define HALL_EFFECT ADC1_CHANNEL_0 //Arbitrary GPIO (ESP32 has ADC on GPI0)
+#define HALL_EFFECT_A ADC1_CHANNEL_0 //Arbitrary GPIO for first Hall Effect Sensor
+#define HALL_EFFECT_B ADC1_CHANNEL_1 //Arbitrary GPIO for second Hall Effect Sensor
 
 static const char *TAG = "MY_MODULE"; //ESPI_LOG Tag 
 int get_rumble_control();
 int get_mode_control();
 float get_hall_effect();
 float get_throttle_speed(int mode_control, float speed_percent);
+
+float get_hall_magnitude(); 
+int get_hall_direction();
 
 #endif
