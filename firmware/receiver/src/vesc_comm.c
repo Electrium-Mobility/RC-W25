29f868d5dc741c4c7a75c5a836844d00aab32abc
VincentQu24 <vincentqu24@gmail.com>
2025-03-05 21:21:12 -0500

    determine_speed() function created

--- firmware/receiver/src/vesc_comm.c
+++ firmware/receiver/src/vesc_comm.c
@@ -1,16 +1,20 @@
 #include "init.h"
 #include "vesc_comm.h"
 #include "VescUart.h"
+#include "math.h"
 
 int boardSpeed;
 int boardBatteryLevel;
 int maxRpm;
+
 
 //Here is where you read the battery level of the longboard
 //Extract inpVoltage from vescData (It's defined in init.h and updated constantly)
 //0% battery is around 3V, max battery is around 5V
 //DON'T scale linearly, figure out how to map these values to a percentage
 //There is a diagram in the doc but it's not perfect for our specifications
+
+
 void read_battery_percent() {
 
 }
@@ -28,7 +32,12 @@ void apply_throttle() {
 //Get the rpm from vescData (It's defined in init.h and updated constantly)
 //MAX_SPEED and WHEEL_DIAMETER are defined in init.h but you can use them here
 void determine_speed() {
-
+    double board_rpm = vescData->rpm;
+    double speed = board_rpm*M_PI*WHEEL_DIAMETER*60/1000
+    if (speed > MAX_SPEED) {
+        speed = MAX_SPEED;
+    }
+    boardSpeed = speed;
 }
 
 void fetchVesc() {
